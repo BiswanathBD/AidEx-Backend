7711c16864b5d61454e1f62ab7ec440c6cd79ce3
BiswanathBD <biswanath.sarker.bd@gmail.com>
2025-12-09 09:07:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("AidEx. Server is Running");
+});
+
+const uri = process.env.MONGO_URI;
+
+// MongoDB
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("✅ Successfully connected to MongoDB!");
+
+    const AidExDB = client.db("AidExDB");
+    const usersCollection = AidExDB.collection("usersCollection");
+
+    // create user
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(user);
+    });
+
+    // get user data
+    app.get("/user", async (req, res) => {
+      const { email } = req.query;
+      console.log(email);
+
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`🚀 AidEx server listening on port ${port}`);
+});
